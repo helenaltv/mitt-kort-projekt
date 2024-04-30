7f44c29cf7ced1c87167a75de60f12b17e270441
Helena Adelsten <helenaltv@gmail.com>
2024-04-30 18:47:09 +0200

    components

---
+++ app/Components/Footer.jsx
@@ -0,0 +1,29 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="F1">
+        <h1>Kontakt</h1>
+        <p>
+          Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är min
+          Footer. Hej! Detta är min Footer.
+        </p>
+      </div>
+      <div className="F2">
+        <h1>Snabbmenu</h1>
+        <p>
+          Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är min
+          Footer. Hej! Detta är min Footer.
+        </p>
+      </div>
+      <div className="F3">
+        <h1>Snabbmenu</h1>
+        <p>
+          Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är min
+          Footer. Hej! Detta är min Footer.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -2,39 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 function Home() {
-  return (
-    <div>
-      <button className="Knapp">
-        <Link className="link" href={"/"}>
-          Skapa
-        </Link>
-      </button>
-      <div className="footer">
-        <div className="F1">
-          <h1>Kontakt</h1>
-          <p>
-            Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är
-            min Footer. Hej! Detta är min Footer.
-          </p>
-        </div>
-        <div className="F2">
-          <h1>Snabbmenu</h1>
-          <p>
-            Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är
-            min Footer. Hej! Detta är min Footer.
-          </p>
-        </div>
-        <div className="F3">
-          <h1>Snabbmenu</h1>
-          <p>
-            Hej! Detta är min Footer. Hej! Detta är min Footer. Hej! Detta är
-            min Footer. Hej! Detta är min Footer.
-          </p>
-        </div>
-      </div>
-      ;
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
